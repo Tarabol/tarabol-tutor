db81e450c23114769bf939cbc8857ecf0a032065
Trung Truong <trungtruong@Trungs-MacBook-Pro.local>
2023-04-02 18:39:30 -0500

    add html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
 
@@ -14,17 +13,18 @@ function App() {
       {
         model: "gpt-3.5-turbo",
         messages: [
-          {"role": "system", "content": `I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is "istanbulu cok seviyom burada olmak cok guzel"`},
+          {"role": "system", "content": `I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations`},
           {"role": "user", "content": `"${input}"`}
         ]
       },
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer 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`,
         },
       }
     )
+    console.log(response);
     return response.data.choices[0].message.content
   }
 
@@ -38,16 +38,27 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h2>Tell me something, and I'll tell you more</h2>
-      <textarea
-        value={input}
-        onChange={(event) => setInput(event.target.value)}
-        rows={5}
-        placeholder="Type in some words and I'll finish the rest..."
-      />
-      <button className="button" onClick={handleClick}>Complete Sentence</button>
-      {completedSentence && <p>Completed sentence: {completedSentence}</p>}
+    <div className="container mx-auto my-5">
+      <div className='flex flex-row'>
+        <textarea 
+          id="message" 
+          rows="7" 
+          className="basis-1/2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2" 
+          placeholder="Type in your sentences, then I will translate to english and make it more elegant" 
+          onChange={(event) => setInput(event.target.value)} value={input}/>
+          <textarea 
+            disabled={true}
+            overflow-y="scroll"
+            id="message" 
+            rows="7" 
+            className='basis-1/2 ml-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            placeholder="Translate result" 
+            value={completedSentence}/>
+      </div>
+      <div className='flex justify-center'>
+        <button className="p-2 my-5 rounded-md bg-indigo-500 hover:bg-cyan-600 text-white" onClick={handleClick}>Complete Sentence</button>
+      </div>
+      
     </div>
   );
 }
